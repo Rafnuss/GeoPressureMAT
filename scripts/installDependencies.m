8b6070327d10c50d1bcde6ad3384e41122b5dee6
Mathieu Gravey <mathieu@mgravey.com>
2021-11-07 21:37:05 +0100

    Create installDependencies.m

---
+++ scripts/installDependencies.m
@@ -0,0 +1,6 @@
+clear all
+websave('../functions/brewermap.m','https://raw.githubusercontent.com/DrosteEffect/BrewerMap/master/brewermap.m');
+websave('../functions/borderdata.mat','https://github.com/chadagreene/CDT/raw/master/cdt/cdt_data/borderdata.mat');
+websave('../functions/borders.m','https://raw.githubusercontent.com/chadagreene/CDT/master/cdt/borders.m');
+websave('../functions/windrose_210420.zip','https://dpereira.asempyme.com/windrose/files/windrose_210420.zip');
+unzip('../functions/windrose_210420','../functions/windrose')
